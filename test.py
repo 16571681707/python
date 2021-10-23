630fa9eae00ec04533ea2806b378f92e236bd7e1
16571681707 <77574453+16571681707@users.noreply.github.com>
2021-10-23 18:29:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 
-
+import re
 findTitle = re.compile('htm">(.*?)</a>')   
 header = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
